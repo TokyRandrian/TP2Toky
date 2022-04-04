0d2bd8b9277963d51b120b0182185d533450b32d
ETU00839 <randrianimanana.tokiniaina@gmail.com>
2022-04-04 09:46:36 +0300

    Fin TP2

---
+++ src/main/java/mg/itu/toky/tp2/config/ConfigJSF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.toky.tp2.config;
+
+/**
+ *
+ * @author ASUS
+ */
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
